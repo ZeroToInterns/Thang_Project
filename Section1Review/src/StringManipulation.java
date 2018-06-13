fbaa08c8516cb899cb94952a6470f210ec6daf0c
Thang Nguyen <thangnguyen@Thangs-MacBook-Pro.local>
2018-06-13 10:54:53 -0700

    Finish Problem 4

---
+++ Section1Review/src/StringManipulation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class StringManipulation {
+	
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Enter your full name: ");
+		String input = kb.nextLine();
+		String space = " ";
+		String firstName = input.substring(0,input.indexOf(space));
+		System.out.println(input.substring(0, 1));
+		System.out.println(firstName);
+		System.out.println(input.substring(input.indexOf(space) + 1, input.length()-1));
+		System.out.println(input.substring(4, 7));
+	}
+
+	
+
+}
